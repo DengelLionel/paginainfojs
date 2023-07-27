7f4d80071660dcc612749a9fe3f49a77383bd71a
DengelLionel <dengelriveracajincho@gmail.com>
2023-07-26 23:31:29 -0500

    ok

--- src/components/Layouts/layauts_two/menumobil/NavBarMobil2.js
+++ src/components/Layouts/layauts_two/menumobil/NavBarMobil2.js
@@ -106,23 +106,25 @@ const NavBarMobil = () => {
                     className={` bg-white  ${
                         openSearch ? 'h-[200px] opacity-100' : 'h-0 opacity-0'
                     }`}>
-                    <div className="pt-[50px] pl-[20px] pr-[20px] flex flex-row gap-[20px] items-center justify-center text-gray-600 relative">
-                        <input
-                            className="border-2 w-full border-gray-300 bg-white h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none"
-                            type="search"
-                            name="search"
-                            value={datobuscar}
-                            onChange={handleInputChange}
-                            placeholder="Buscar"
-                        />
+                    {openSearch && (
+                        <div className="pt-[50px] pl-[20px] pr-[20px] flex flex-row gap-[20px] items-center justify-center text-gray-600 relative">
+                            <input
+                                className="border-2 w-full border-gray-300 bg-white h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none"
+                                type="search"
+                                name="search"
+                                value={datobuscar}
+                                onChange={handleInputChange}
+                                placeholder="Buscar"
+                            />
 
-                        <button
-                            onClick={handleSearch}
-                            type="button"
-                            className="absolute right-[10%] top-[75%] md:right-[5%] transform -translate-y-1/2 bg-transparent w-[30px] h-[30px]">
-                            <IconBuscador fill="#2F6AAD" />
-                        </button>
-                    </div>
+                            <button
+                                onClick={handleSearch}
+                                type="button"
+                                className="absolute right-[10%] top-[75%] md:right-[5%] transform -translate-y-1/2 bg-transparent w-[30px] h-[30px]">
+                                <IconBuscador fill="#2F6AAD" />
+                            </button>
+                        </div>
+                    )}
                 </div>
             </div>
         </nav>
