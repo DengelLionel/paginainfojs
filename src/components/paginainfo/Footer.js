1849d346a9c6f8cbcc8e3aae700523dcaa9d0b67
DengelLionel <dengelriveracajincho@gmail.com>
2024-01-06 16:01:41 -0500

    right

--- src/components/paginainfo/Footer.js
+++ src/components/paginainfo/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
     return (
         <div className="bg-blueTwo text-gray-600 font-normal font-notosans text-[11px] flex flex-col items-center justify-center mt-[18px] pt-[22px] pb-[22px] lg:flex-row lg:items-center lg:pb-[10px] lg:pt-[15px]">
             <span className="text-center w-[314px] mt-[10px] lg:w-[400px]  lg:mt-0">
-                NEXOMEDIC S.A.C. TODOS LOS DERECHOS RESERVADOS, Copyright © 2023
+                NEXOMEDIC S.A.C. TODOS LOS DERECHOS RESERVADOS, Copyright © 2024
             </span>
         </div>
     )
